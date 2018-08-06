33f5b4bc26307aefdce58b2add7ce36522dc2222
bi-weleda <bilalw38@gmail.com>
2018-08-06 19:01:44 +0500

    models ..

--- trackerapp/admin.py
+++ trackerapp/admin.py
@@ -1,6 +1,30 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from trackerapp.models import TicketTracking,Bug,Feature
+
+
+class TicketTrackingAdmin(admin.ModelAdmin):
+    model = TicketTracking
+    list_display = ('ticket_name', 'ticket_comment', 'ticket_status')
+
+class BugAdmin(admin.ModelAdmin):
+    model = Bug
+    list_display = ('bug_name', 'description', 'solution')
+
+class FeatureAdmin(admin.ModelAdmin):
+    model = Feature
+    list_display = ('feature_name')
+
+
+
+admin.site.register(TicketTracking,TicketTrackingAdmin)
+admin.site.register(Bug,BugAdmin)
+admin.site.register(Feature)
+
+
+
+
+
+
--- trackerapp/models.py
+++ trackerapp/models.py
@@ -1,6 +1,26 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class TicketTracking(models.Model):
+    ticket_name = models.CharField(max_length=250)
+    ticket_comment = models.CharField(max_length=250)
+    ticket_status = models.CharField(max_length=250)
+
+class Bug(models.Model):
+    bug_name = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    solution = models.CharField(max_length=250)
+
+class Feature(models.Model):
+    feature_name = models.CharField(max_length=250)
+
+
+
+
+
+
+
+
+
+
+
